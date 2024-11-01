649acad850b0a20691d9f2136c76b1366cdaf1db
elly.manuh <141268921+ellycristyne@users.noreply.github.com>
2024-11-01 05:36:39 -0300

    tvnjk

---
+++ perguntas.js
@@ -0,0 +1,126 @@
+criaCartao(
+    'Português',
+    'Quem escreveu "Dom Casmurro"?',
+    'Machado de Assis.'
+)
+
+criaCartao(
+    'História',
+    'Em que ano começou a Segunda Guerra Mundial?',
+    'Iniciou em 1939.'
+)
+
+criaCartao(
+    'Filosofia',
+    'Quem é conhecido como o "pai da filosofia ocidental"?  ',
+    'Sócrates.'
+)
+
+
+criaCartao(
+    'Biologia',
+    'O que é Fotossíntese?',
+    'É o processo pelo qual as plantas convertem luz solar em energia química, utilizando dióxido de carbono e água.'
+)
+
+criaCartao(
+    'Física',
+    'O que é a lei da gravitação universal? ',
+    'É a lei que afirma que dois corpos se atraem com uma força que é proporcional ao produto de suas massas e inversamente proporcional ao quadrado da distância entre eles.'
+)
+
+criaCartao(
+    'Química',
+    'O que é um ácido?',
+    'É uma substância que libera íons de hidrogênio (H⁺) em solução aquosa.'
+)
+
+criaCartao(
+    'Artes',
+    'O que caracteriza o movimento impressionista na pintura?',
+    'A ênfase na luz, na cor e nas impressões momentâneas, ao invés de detalhes precisos.'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'O que é a biodiversidade?',
+    'É a variedade de formas de vida em um determinado ambiente ou no planeta como um todo.'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que é inflação?',
+    'É o aumento generalizado dos preços de bens e serviços em uma economia.'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 4x3?',
+    '12.'
+)
+
+criaCartao(
+    'Programação.',
+    'O que é a inteligência artificial? ',
+    'É a capacidade de máquinas e sistemas computacionais de realizar tarefas que normalmente requerem inteligência humana.'
+)
+
+criaCartao(
+    'Sociologia',
+    'O que é cultura? ',
+    'É o conjunto de valores, normas, costumes e práticas que caracterizam um grupo social.'
+)
+
+criaCartao(
+    'Educação Física',
+    'O que é a frequência cardíaca em repouso? ',
+    'É a quantidade de batimentos cardíacos por minuto quando a pessoa está em estado de repouso.'
+)
+
+criaCartao(
+    'Inglês',
+    'O que é "Good Morning" em português?',
+    'Bom Dia.'
+)
+
+criaCartao(
+    'Projeto de Vida',
+    'O que é resiliência? ',
+    'Resiliência é a capacidade de uma pessoa se recuperar de adversidades, enfrentar desafios e se adaptar a situações difíceis.'
+)
+
+criaCartao(
+    'Empreendedorismo',
+    'Qual é a diferença entre empreendedorismo social e empreendedorismo corporativo? ',
+    'O empreendedorismo social foca em resolver problemas sociais e ambientais, buscando impacto positivo, enquanto o empreendedorismo corporativo ocorre dentro de empresas, visando inovações que gerem lucro e competitividade.'
+)
+
+criaCartao(
+    'Português',
+    'O que é um adjetivo?',
+    'É uma palavra que descreve ou qualifica um substantivo.'
+)
+
+criaCartao(
+    'Física',
+    'Qual é o planeta mais próximo do Sol? ',
+    ' Mercúrio.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o autor da teoria da evolução das espécies e em que obra ele a apresentou? ',
+    'Charles Darwin, na obra "A Origem das Espécies".'
+)
+
+criaCartao(
+    'Geografia',
+    ' Qual é o país com a maior área territorial do mundo? ',
+    'A Rússia.'
+)
+
+criaCartao(
+    'Química',
+    'O que é um elemento químico? ',
+    'É uma substância que não pode ser decomposta em substâncias mais simples e é composta por átomos que têm o mesmo número de prótons no núcleo.'
+)
